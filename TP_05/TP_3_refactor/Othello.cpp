cb22d9ff5e9b6fbc7435d50f1965ff578f0ce749
bastianfbr <badmaxyt@gmail.com>
2022-12-21 23:57:41 +0100

    delete: emptying useless function

--- TP_05/TP_3_refactor/Othello.cpp
+++ TP_05/TP_3_refactor/Othello.cpp
@@ -27,57 +27,16 @@ bool Othello::addToken(const int _playerId, const int _column, const int _lines)
 
 bool Othello::isLineFull(const Player& _player) const
 {
-	for (int i = 0; i < LINES_NUMBER; i++)
-	{
-		for (int j = 0; j < COLUMNS_NUMBER - 3; j++)
-		{
-			if (grid[i][j] == _player.getId() && grid[i][j + 1] == _player.getId() && grid[i][j + 2] == _player.getId() && grid[i][j + 3] == _player.getId())
-			{
-				return true;
-			}
-		}
-	}
 	return false;
 }
 
 bool Othello::isColumnFull(const Player& _player) const
 {
-	for (int j = 0; j < COLUMNS_NUMBER; j++)
-	{
-		for (int i = 0; i < LINES_NUMBER - 3; i++)
-		{
-			if (grid[i][j] == _player.getId() && grid[i + 1][j] == _player.getId() && grid[i + 2][j] == _player.getId() && grid[i + 3][j] == _player.getId())
-			{
-				return true;
-			}
-		}
-	}
 	return false;
 }
 
 bool Othello::isDiagonalFull(const Player& _player) const
 {
-	for (int i = 0; i < LINES_NUMBER - 3; i++)
-	{
-		for (int j = 0; j < COLUMNS_NUMBER - 3; j++)
-		{
-			if (grid[i][j] == _player.getId() && grid[i + 1][j + 1] == _player.getId() && grid[i + 2][j + 2] == _player.getId() && grid[i + 3][j + 3] == _player.getId())
-			{
-				return true;
-			}
-		}
-	}
-
-	for (int i = COLUMNS_NUMBER - 1; i > 2; i--)
-	{
-		for (int j = 0; j < LINES_NUMBER - 3; j++)
-		{
-			if (grid[j][i] == _player.getId() && grid[j + 1][i - 1] == _player.getId() && grid[j + 2][i - 2] == _player.getId() && grid[j + 3][i - 3] == _player.getId())
-			{
-				return true;
-			}
-		}
-	}
 	return false;
 }
 
